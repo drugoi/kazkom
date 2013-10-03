cbd0a58c5f58dbcd2c80c6c6ccf8f060c633aa12
Nikita Bayev <drugoi.nik@gmail.com>
2013-10-03 16:11:46 +0600

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+/* borschik:include:jquery.min.js */
+/* borschik:include:requestAnimationFrame.js */
+/* borschik:include:jquery.parallax.js */
+/* borschik:include:mask.js */
+/* borschik:include:affix.js */
+/* borschik:include:app.js */
