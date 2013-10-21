56648f138dfa2003bff6f09495647315fb7402ed
Nikita Bayev <drugoi.nik@gmail.com>
2013-10-21 21:31:51 +0600

    Added new features to project

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/* Toggle Attr jQuery */
+jQuery.fn.toggleAttr = function(attr) {
+ return this.each(function() {
+  var $this = $(this);
+  $this.attr(attr) ? $this.removeAttr(attr) : $this.attr(attr, attr);
+ });
+};
